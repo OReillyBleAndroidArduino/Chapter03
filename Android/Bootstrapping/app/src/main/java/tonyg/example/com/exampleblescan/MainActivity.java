0829a315782b7e8d56ce80867157ca18d9df5ec5
Adonis Gaitatzis <backupbrain@gmail.com>
2016-11-14 20:10:09 -0800

    typo fixes

--- Android/Bootstrapping/app/src/main/java/tonyg/example/com/exampleblescan/MainActivity.java
+++ Android/Bootstrapping/app/src/main/java/tonyg/example/com/exampleblescan/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import tonyg.example.com.exampleblescan.ble.BleCommManager;
@@ -27,8 +28,13 @@ public class MainActivity extends AppCompatActivity {
     /** Bluetooth Stuff **/
     private BleCommManager mBleCommManager;
 
+    /** UI Stuff **/
+    private TextView mBluetoothStatusTV;
 
-
+    /**
+     * Load Activity for the first time
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +43,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        loadUI();
+
     }
 
+    private void loadUI() {
+        mBluetoothStatusTV = (TextView) findViewById(R.id.bluetooth_status);
+    }
 
     /**
      * Turn on Bluetooth Radio notifications when App resumes
@@ -48,6 +59,16 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         initializeBluetooth();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            unregisterReceiver(mBluetoothBroadcastReceiver);
+        } catch (IllegalArgumentException e) {
+            Log.e(TAG, "receiver not registered");
+        }
     }
 
 
@@ -65,9 +86,22 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
+     * Bluetooth Radio has been turned on.  Update UI
+     */
+    private void onBluetoothActive() {
+        mBluetoothStatusTV.setText(R.string.bluetooth_active);
+    }
+
+
+    /**
      * Initialize the Bluetooth Radio
      */
     public void initializeBluetooth() {
+
+        // notify when bluetooth is turned on or off
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(mBluetoothBroadcastReceiver, filter);
+
         try {
             mBleCommManager = new BleCommManager(this);
         } catch (Exception e) {
@@ -77,12 +111,51 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // should prompt user to open settings if Bluetooth is not enabled.
-        if (!mBleCommManager.getBluetoothAdapter().isEnabled()) {
+        if (mBleCommManager.getBluetoothAdapter().isEnabled()) {
+            onBluetoothActive();
+        } else {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
     }
+
+
+
+
+    /**
+     * When the Bluetooth radio turns on, initialize the Bluetooth connection
+     */
+    private final BroadcastReceiver mBluetoothBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.ERROR);
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        initializeBluetooth();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                onBluetoothActive();
+                            }
+                        });
+
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        break;
+                }
+            }
+        }
+    };
 
 
 
